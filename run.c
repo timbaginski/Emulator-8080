109e806e0475c0e59554c496ad23d1431db0f019
Baginski Timothy <timbaginski3@gmail.com>
2021-07-16 10:27:23 -0400

    working on main

--- run.c
+++ run.c
@@ -46,7 +46,38 @@ void load_invaders_chunk(char *folder, char chunk, uint8_t *memory) {
 
 } 
 
+void load_invaders(uint8_t *memory, char *folder) {
+  // load each chunk of invaders into memory
+  load_invaders_chunk(folder, 'h', memory); 
+  load_invaders_chunk(folder, 'g', memory); 
+  load_invaders_chunk(folder, 'f', memory); 
+  load_invaders_chunk(folder, 'e', memory);
+
+}
+
 int main(){
+  // initialize state
+  State8080 *state; 
+  state = (State8080 *) malloc(sizeof(State8080 *)); 
+  state->a = 0; 
+  state->b = 0; 
+  state->c = 0; 
+  state->d = 0; 
+  state->e = 0;
+  state->h = 0; 
+  state->l = 0; 
+  state->sp = 0; 
+  state->pc = 0; 
+  state->memory = (uint8_t *) malloc((1 << 15) * sizeof(uint8_t *)); 
+  state->int_enable = 0; 
+  // initialize flags
+  state->cc.z = 0; 
+  state->cc.s = 0; 
+  state->cc.p = 0; 
+  state->cc.cy = 0; 
+  state->cc.ac = 0; 
+
+
   return 0;
 }
 
